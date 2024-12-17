c293c1b1bdd0a960afcf58a1128443e3a605ebf3
blow <blowuppp@gmail.com>
2024-12-17 13:41:30 +0000

    revise lookonce more

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,25 @@ import { useEffect } from 'react';
 export default function Home() {
   useEffect(() => {
     const body = document.body;
-    let hue = 210; // Starting hue for a blue color
+    const colorStart = { r: 14, g: 162, b: 181 }; // #0ea2b5
+    const colorEnd = { r: 100, g: 196, b: 209 }; // #64c4d1
+    let lightness = 70; // Starting lightness
+    let increasing = true; // Flag to determine if lightness is increasing or decreasing
     const updateBackground = () => {
-      hue = (hue + 1) % 240; // Limit hue to a range of blue colors
-      body.style.background = `hsl(${hue}, 70%, 80%)`;
+      if (increasing) {
+        lightness += 1; // Increase lightness
+        if (lightness >= 80) increasing = false; // Change direction if max is reached
+      } else {
+        lightness -= 1; // Decrease lightness
+        if (lightness <= 60) increasing = true; // Change direction if min is reached
+      }
+
+      // Interpolate between the two colors
+      const r = Math.round(colorStart.r + (colorEnd.r - colorStart.r) * ((lightness - 60) / 20));
+      const g = Math.round(colorStart.g + (colorEnd.g - colorStart.g) * ((lightness - 60) / 20));
+      const b = Math.round(colorStart.b + (colorEnd.b - colorStart.b) * ((lightness - 60) / 20));
+
+      body.style.background = `rgb(${r}, ${g}, ${b})`;
     };
     const interval = setInterval(updateBackground, 100);
     return () => clearInterval(interval);
@@ -25,14 +40,17 @@ export default function Home() {
 
       <main>
         <div className="content-wrapper">
-          <Header title="Hello customers  Please review RE & CO" />
+          <img src="/hairlogo.png" alt="Hair Logo" style={{ display: 'block', margin: '0 auto', maxWidth: '100%', height: 'auto' }} />
           <div className="contact-box">
+            <a href="#" className="contact-link" style={{ display: 'block', textAlign: 'center' }}>
+              Hello customers
+            </a>
             <a href="https://www.google.com/search?q=stopandstyle_+sevenoaks&rlz=1C5CHFA_enGB1100GB1100&oq=stopandstyle_+sevenoaks&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEAAYFhgeMggIAxAAGBYYHjIICAQQABgWGB7SAQkxNDc4OGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#lrd=0x47df4de7e538bd5b:0x21655eb46d4fee78,3,,,,," 
                target="_blank" 
                rel="noopener noreferrer" 
                className="contact-link">
-              <img src="/google_g_icon_download.png" alt="Google Reviews" className="social-icon" />
-              <span>Our Google Reviews</span>
+              <img src="/google_g_icon_download.png" alt="Google Reviews" className="social-icon" style={{ width: '24px', height: '24px', marginRight: '8px' }} />
+              Our Google Reviews
             </a>
             <a href="https://www.instagram.com/stopandstyle_?igsh=NTc4MTIwNjQ2YQ==" 
                target="_blank" 
