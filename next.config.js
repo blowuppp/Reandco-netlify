466f54df4db92285a690b07277a179e0ca5b3699
blowuppp <mark@mgv.co.uk>
2025-02-10 11:18:49 +0000

    NetlifyCreate0.2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-};
 
-module.exports = nextConfig;
+
+const path = require('path');
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/static/admin/index.html',
+      },
+    ];
+  },
+};
