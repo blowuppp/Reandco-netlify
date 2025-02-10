bac25357066bfc758a5ea5f9ad0a6402bc5a3145
blowuppp <mark@mgv.co.uk>
2025-02-10 10:16:07 +0000

    Home text update

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Awesome Hair By RE & CO!</title>
+        <title>Splendid Hair By RE & CO!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
